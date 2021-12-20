0397d8805f2c2f55dca7a7a39e5493474a7a3ba8
Juloass <Juloass@gmail.com>
2021-12-20 03:01:48 +0100

    Microsoft & New UI (Thx to ModRealms Version)
    > Title

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
-const APP_DATA_NAME = 'ModRealmsLauncher'
-const LIVE_DISTRIBUTION_URL = 'https://launcher.modrealms.net/deploy/distribution.json'
-const DEV_DISTRIBUTION_URL = 'https://launcher.modrealms.net/dev-deploy/distribution.json'
+const APP_DATA_NAME = 'InkubateLauncher'
+const LIVE_DISTRIBUTION_URL = 'https://launcher.inkubate.net/deploy/distribution.json'
+const DEV_DISTRIBUTION_URL = 'https://launcher.inkubate.net/dev-deploy/distribution.json'
 
 module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL }
